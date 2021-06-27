f242c7bed30fe68674e7c0a725d1b30cc930a5f5
Duenize <duedav@gmail.com>
2021-06-27 02:26:30 +0100

    First commit

---
+++ week1.php
@@ -0,0 +1,18 @@
+<?php
+
+// sum question.
+$sum= 10;
+$sum +=25;//+= is same as taking two numbers and adding them
+echo $sum;
+
+
+
+//range
+$num =range(0,10); // foreach makes the $num to be echoed as $r
+foreach ($num as $r){
+	echo "$r";
+}
+
+
+
+?>
